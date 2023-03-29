e1d0bd27cc53994dbec762a317075be67eaaca54
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-29 21:19:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ price.forEach((item)=>{
 console.log(sum);
 let tr = document.createElement("tr");
  let table = document.getElementById("table");
-tr.innerHTML="<td>"+sum+"</td>";
+tr.innerHTML="<td colspan='2'>"+sum+"</td>";
 table.appendChild(tr);
