5cf201ad6ed093ee3dfcdd89246f7cc818a7dd6a
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-29 21:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let price =document.querySelectorAll("tr td:nth-child(2)");
+let sum = 0;
+price.forEach((item)=>{
+	// console.log(item.innerText);
+	sum += +item.innerText;	
+})
+console.log(sum);
+let tr = document.createElement("tr");
+ let table = document.getElementById("table");
+tr.innerHTML="<td>"+sum+"</td>";
+table.appendChild(tr);
