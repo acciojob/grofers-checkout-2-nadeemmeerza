96a1c514b7be8f32a4a61ca501dc2691d7d924cd
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-01 22:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your code here
 // let price =document.querySelectorAll("tr td:nth-child(2)");
-let price =document.querySelectorAll('[data-ns-test="price"]');
+let price =document.querySelectorAll('[data-ns-test="prices"]');
 let sum = 0;
 price.forEach((item)=>{
 	// console.log(item.innerText);
